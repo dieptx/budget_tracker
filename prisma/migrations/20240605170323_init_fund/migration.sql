549bd01c7621aa742d17f48ab2b36b0255f2185d
dieptx <dieptx.ptit@gmail.com>
2024-06-06 00:07:01 +0700

    chore: init fund

---
+++ prisma/migrations/20240605170323_init_fund/migration.sql
@@ -0,0 +1,6 @@
+-- Custom SQL migration file, put you code below! --
+INSERT INTO "Fund" (name, description) VALUES 
+('EveryDay Fund', 'Quỹ hàng ngày'),
+('Emergency Fund', 'Quỹ khẩn cấp'),
+('Sinking Fund', 'Quỹ kế hoạch'),
+('Investment Fund', 'Quỹ đầu tư');
